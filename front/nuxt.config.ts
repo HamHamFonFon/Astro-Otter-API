89a8249548322fa7781e4dfd9a7408cce0b2a3c8
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-02-14 15:07:33 +0100

    Ini nuxt front

---
+++ front/nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // astrobin
+
+    // login/pwd API
+
+    // Values exposed public side
+    public: {
+
+    }
+  }
+})
