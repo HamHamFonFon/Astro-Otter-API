9d882e4d6126f0a1ca358cde9271151ac65d04ab
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-03-04 12:51:38 +0100

    OpenAI secret key

--- api/src/State/SearchByAiProcessor.php
+++ api/src/State/SearchByAiProcessor.php
@@ -5,12 +5,12 @@ namespace App\State;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Search;
-use Gemini\Client;
+
 
 readonly class SearchByAiProcessor implements ProcessorInterface
 {
     public function __construct(
-        private string $apiGeminiKey
+        private string $apiOpenAI
     )
     { }
 
@@ -19,9 +19,5 @@ readonly class SearchByAiProcessor implements ProcessorInterface
         if (!$data instanceof Search) {
             throw new \Error();
         }
-        $content = $data->getTerms();
-        $client = \Gemini::client($this->apiGeminiKey);
-
-        $result = $client->geminiPro()->generateContent($content);
     }
 }
