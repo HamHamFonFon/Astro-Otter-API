1c9623bfa3f5697b12750a810ddc618f3c61c04d
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-03-05 10:40:18 +0100

    One migration

--- api/migrations/Version20240305093943.php
+++ api/migrations/Version20240305093943.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240227113435 extends AbstractMigration
+final class Version20240305093943 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,22 @@ final class Version20240227113435 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE refresh_tokens_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('DROP SEQUENCE api_user_id_seq CASCADE');
+        $this->addSql('CREATE TABLE api_user (id UUID NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, is_active BOOLEAN DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_AC64A0BAE7927C74 ON api_user (email)');
+        $this->addSql('COMMENT ON COLUMN api_user.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE refresh_tokens (id INT NOT NULL, refresh_token VARCHAR(128) NOT NULL, username VARCHAR(255) NOT NULL, valid TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_9BACE7E1C74F2195 ON refresh_tokens (refresh_token)');
+        $this->addSql('CREATE TABLE update_data (id INT NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, list_dso JSON NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE refresh_tokens_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE api_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('DROP TABLE api_user');
         $this->addSql('DROP TABLE refresh_tokens');
+        $this->addSql('DROP TABLE update_data');
     }
 }
