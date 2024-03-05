18277d215431d6c58fa461bd2993af93e388b9d6
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-03-05 10:32:33 +0100

    remove useless sql command

--- api/migrations/Version20240226151716.php
+++ api/migrations/Version20240226151716.php
@@ -20,13 +20,11 @@ final class Version20240226151716 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP SEQUENCE greeting_id_seq CASCADE');
         $this->addSql('CREATE SEQUENCE api_user_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE update_data_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE api_user (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_AC64A0BAE7927C74 ON api_user (email)');
         $this->addSql('CREATE TABLE IF NOT EXISTS update_data (id INT NOT NULL, date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, list_dso JSON NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('DROP TABLE IF EXISTS greeting');
     }
 
     public function down(Schema $schema): void
@@ -35,8 +33,6 @@ final class Version20240226151716 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE api_user_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE update_data_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE greeting_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE greeting (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('DROP TABLE api_user');
         $this->addSql('DROP TABLE update_data');
     }
