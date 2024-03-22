60e2ebee1969d01349477c799f5a8493ca9a214a
Stephane MEAUDRE <smeaudre@kaliop.com>
2024-03-22 14:19:15 +0100

    Add route attribute on homepage

--- api/src/Controller/Home.php
+++ api/src/Controller/Home.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
 
 class Home extends AbstractController
 {
+    #[Route(path: '/', name: 'home')]
     public function __invoke(Request $request, string $apiDocUrl): RedirectResponse
     {
         return $this->redirect($apiDocUrl);
